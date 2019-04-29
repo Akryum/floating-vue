d60513c64c266f0c1e926e2e583a5733819138bd
Guillaume Chau <alphadelta.fg@gmail.com>
2019-04-29 15:33:26 +0200

    chore: add rollup analyzer

--- build/rollup.config.base.js
+++ build/rollup.config.base.js
@@ -4,6 +4,7 @@ import vue from 'rollup-plugin-vue'
 import cjs from 'rollup-plugin-commonjs'
 import replace from 'rollup-plugin-replace'
 import postcss from 'rollup-plugin-postcss'
+import analyze from 'rollup-plugin-analyzer'
 
 const config = require('../package.json')
 
@@ -26,6 +27,7 @@ export default {
       VERSION: JSON.stringify(config.version),
     }),
     postcss(),
+    analyze(),
   ],
   watch: {
     include: 'src/**',
