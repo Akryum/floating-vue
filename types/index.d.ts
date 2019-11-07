14e045036df2fd906ab49fddb8e08ad8c086c9fd
Yoyo Young <yoyoys@livemail.tw>
2019-11-08 07:32:31 +0800

    chore: update types

---
+++ types/index.d.ts
@@ -0,0 +1,8 @@
+import Vue, { VueConstructor, DirectiveOptions, PluginFunction } from 'vue';
+
+const vToolTip: PluginFunction<any>;
+export default vToolTip;
+
+export const VPopover: VueConstructor<Vue>;
+export const VClosePopover: DirectiveOptions;
+export const VTooltip: DirectiveOptions;
