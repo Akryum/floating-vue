5f1d7b9707a70be061012cc34ca2181c943f52c2
Faruk <faarukcan@gmail.com>
2020-01-29 10:58:25 +0300

    Export added for top level declaration
    Tslint says: Top-level declarations in .d.ts files must start with either a 'declare' or 'export' modifier.
    
    This PR resolve this issue adding export to top level declaration in d.ts file.

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,6 +1,6 @@
 import Vue, { VueConstructor, DirectiveOptions, PluginFunction } from 'vue';
 
-const vToolTip: PluginFunction<any>;
+export const vToolTip: PluginFunction<any>;
 export default vToolTip;
 
 export const VPopover: VueConstructor<Vue>;
