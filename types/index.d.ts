5bcaf14baa5d9677b2761322f63413e6a4be37de
okandas <okandas@users.noreply.github.com>
2020-10-14 10:16:17 +0200

    Update index.d.ts

--- types/index.d.ts
+++ types/index.d.ts
@@ -3,6 +3,6 @@ import Vue, { VueConstructor, DirectiveOptions, PluginFunction } from 'vue';
 
 export const VPopover: VueConstructor<Vue>;
 export const VClosePopover: DirectiveOptions;
-export const VTooltip: PluginFunction<any> | DirectiveOptions;
+export const VToolTip: PluginFunction<any> | DirectiveOptions;
 
 export default vToolTip;
