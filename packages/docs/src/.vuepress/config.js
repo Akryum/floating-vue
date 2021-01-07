495382943e828e82046930954631bbd633e1c918
Guillaume Chau <guillaume.b.chau@gmail.com>
2021-01-07 11:47:53 +0100

    docs: favicon

--- packages/docs/src/.vuepress/config.js
+++ packages/docs/src/.vuepress/config.js
@@ -18,7 +18,8 @@ module.exports = {
   head: [
     ['meta', { name: 'theme-color', content: '#3eaf7c' }],
     ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
-    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+    ['link', { rel: 'icon', href: '/favicon.png' }]
   ],
 
   /**
