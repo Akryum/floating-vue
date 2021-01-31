46cba72f04fac2019f249b7e9c5978ea70d403a3
Guillaume Chau <guillaume.b.chau@gmail.com>
2021-01-31 13:07:21 +0100

    fix: eagerMount for tooltip directive

--- packages/v-tooltip/src/components/TooltipDirective.vue
+++ packages/v-tooltip/src/components/TooltipDirective.vue
@@ -4,7 +4,7 @@
     v-slot="{
       popperId,
       isShown,
-      isMounted,
+      shouldMountContent,
       skipTransition,
       autoHide,
       hide,
@@ -27,7 +27,7 @@
       :popper-id="popperId"
       :theme="theme"
       :is-shown="isShown"
-      :is-mounted="isMounted"
+      :is-mounted="shouldMountContent"
       :skip-transition="skipTransition"
       :auto-hide="autoHide"
       :handle-resize="handleResize"
