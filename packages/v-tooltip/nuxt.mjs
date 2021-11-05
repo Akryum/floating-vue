b08f0a8ef06a685cd7a9d90b322b9ce290564ba9
Daniel Roe <daniel@roe.dev>
2021-11-05 13:45:59 +0000

    refactor: remove kit dependency

--- packages/v-tooltip/nuxt.mjs
+++ packages/v-tooltip/nuxt.mjs
@@ -1,30 +1,25 @@
-import { defineNuxtModule, addPluginTemplate } from '@nuxt/kit'
-// import { name, version } from './package.json'
+export default function () {
+  const nuxt = this.nuxt
+  nuxt.options.vue.compilerOptions.directiveTransforms = nuxt.options.vue.compilerOptions.directiveTransforms || {}
+  nuxt.options.vue.compilerOptions.directiveTransforms.tooltip = () => ({
+    props: [],
+    needRuntime: true,
+  })
 
-export default defineNuxtModule({
-  name: 'v-tooltip',
-  setup (_options, nuxt) {
-    nuxt.options.vue.compilerOptions.directiveTransforms = nuxt.options.vue.compilerOptions.directiveTransforms || {}
-    nuxt.options.vue.compilerOptions.directiveTransforms.tooltip = () => ({
-      props: [],
-      needRuntime: true,
-    })
+  nuxt.options.css.push('v-tooltip/dist/v-tooltip.css')
 
-    nuxt.options.css.push('v-tooltip/dist/v-tooltip.css')
+  this.addPlugin({
+    filename: 'v-tooltip.mjs',
+    getContents: () => `
+      import { defineNuxtPlugin } from '#app'
+      import VTooltip from 'v-tooltip'
+      
+      export default defineNuxtPlugin((nuxtApp) => {
+        nuxtApp.vueApp.use(VTooltip)
+      })
+    `,
+  })
 
-    addPluginTemplate({
-      filename: 'v-tooltip.mjs',
-      getContents: () => `
-        import { defineNuxtPlugin } from '#app'
-        import VTooltip from 'v-tooltip'
-        
-        export default defineNuxtPlugin((nuxtApp) => {
-          nuxtApp.vueApp.use(VTooltip)
-        })
-        `,
-    })
-
-    // @TODO remove when popper supports native ESM
-    nuxt.options.build.transpile.push('v-tooltip', '@popperjs/core')
-  },
-})
+  // @TODO remove when popper supports native ESM
+  nuxt.options.build.transpile.push('v-tooltip', '@popperjs/core')
+}
