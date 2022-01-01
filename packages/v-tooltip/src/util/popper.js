6f54d1bd270b60e7666e4bc598a6b2745332dc1e
Guillaume Chau <guillaume.b.chau@gmail.com>
2022-01-01 20:33:46 +0100

    fix: applyModifier

--- packages/v-tooltip/src/util/popper.js
+++ packages/v-tooltip/src/util/popper.js
@@ -1,13 +1,21 @@
-export function applyModifier (modifiers, name, options) {
+export function applyModifier (modifiers, name, data) {
   let modifier = modifiers.find(m => m.name === name)
+
   if (!modifier) {
     modifier = {
       name,
-      options: {},
     }
     modifiers.push(modifier)
-  } else if (!modifier.options) {
-    modifier.options = {}
   }
-  Object.assign(modifier.options, options)
+
+  for (const key in data) {
+    if (key === 'options') {
+      if (!modifier.options) {
+        modifier.options = {}
+      }
+      Object.assign(modifier.options, data.options)
+    } else {
+      modifier[key] = data[key]
+    }
+  }
 }
