a76c340bec49de3f851b98ee1a73992909681936
Guillaume Chau <guillaume.b.chau@gmail.com>
2022-01-10 17:55:38 +0100

    chore: zoom transition demo

--- packages/demo-vue3/src/views/component/DropdownDemo2Manual.vue
+++ packages/demo-vue3/src/views/component/DropdownDemo2Manual.vue
@@ -2,6 +2,7 @@
 import { ref } from 'vue'
 
 const shown = ref(false)
+const placement = ref('bottom')
 </script>
 
 <template>
@@ -12,15 +13,27 @@ const shown = ref(false)
       <input v-model="shown" type="checkbox">
       Show dropdown
     </label>
+
+    <select v-model="placement">
+      <option v-for="option of ['bottom', 'top', 'left', 'right']"
+        :key="option"
+        :value="option"
+      >
+        {{ option }}
+      </option>
+    </select>
   </div>
 
   <VDropdown
     v-model:shown="shown"
     :triggers="[]"
     :auto-hide="false"
+    :placement="placement"
     :style="{
       marginLeft: '400px',
     }"
+    popper-class="zoom-in"
+    compute-transform-origin
   >
     <button id="dropdown-btn">Click me</button>
 
@@ -35,5 +48,16 @@ const shown = ref(false)
 <style lang="postcss" scoped>
 .my-menu {
   padding: 20px;
+}
+</style>
+
+<style>
+.v-popper__popper.zoom-in.v-popper__popper--show-from .v-popper__wrapper {
+  transform: scale(.5);
+}
+
+.v-popper__popper.zoom-in.v-popper__popper--show-to .v-popper__wrapper {
+  transform: none;
+  transition: transform .15s;
 }
 </style>
