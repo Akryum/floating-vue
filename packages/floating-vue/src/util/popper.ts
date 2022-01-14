70647574b817db0bf0e58008efa3c1291f82fb35
Guillaume Chau <guillaume.b.chau@gmail.com>
2022-01-15 00:39:51 +0100

    fix: remove applyModifier

--- packages/floating-vue/src/util/popper.ts
+++ packages/floating-vue/src/util/popper.ts
@@ -1,27 +1,5 @@
 import type { Placement as BasePlacement } from '@floating-ui/dom'
 
-export function applyModifier (modifiers, name, data) {
-  let modifier = modifiers.find(m => m.name === name)
-
-  if (!modifier) {
-    modifier = {
-      name,
-    }
-    modifiers.push(modifier)
-  }
-
-  for (const key in data) {
-    if (key === 'options') {
-      if (!modifier.options) {
-        modifier.options = {}
-      }
-      Object.assign(modifier.options, data.options)
-    } else {
-      modifier[key] = data[key]
-    }
-  }
-}
-
 export type Placement = BasePlacement | 'auto' | 'auto-start' | 'auto-end'
 
 export const placements: Placement[] = ['auto', 'top', 'bottom', 'left', 'right'].reduce((acc, base) => acc.concat([
