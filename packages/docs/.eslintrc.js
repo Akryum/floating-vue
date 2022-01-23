69ebe275968ef7db1b9ad135db5f34acba0c4b00
Guillaume Chau <guillaume.b.chau@gmail.com>
2022-01-23 19:53:06 +0100

    chore: fix eslint rule

--- packages/docs/.eslintrc.js
+++ packages/docs/.eslintrc.js
@@ -12,6 +12,7 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'comma-dangle': ['error', 'always-multiline'],
+    quotes: ['error', 'single', { allowTemplateLiterals: true }],
   },
   parserOptions: {
     parser: 'babel-eslint',
